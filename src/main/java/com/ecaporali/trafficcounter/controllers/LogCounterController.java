34a8d0883d7dd36bb96e40a35820a4b444a4781b
Enrico Caporali <enrico.caporali@gmail.com>
2019-11-17 21:39:42 +1100

    Renamed TrafficCounterController.java to LogCounterController.java

--- src/main/java/com/ecaporali/trafficcounter/controllers/LogCounterController.java
+++ src/main/java/com/ecaporali/trafficcounter/controllers/LogCounterController.java
@@ -1,33 +1,31 @@
 package com.ecaporali.trafficcounter.controllers;
 
-import com.ecaporali.trafficcounter.config.AppConfig;
 import com.ecaporali.trafficcounter.models.CalculationResult;
 import com.ecaporali.trafficcounter.models.LogCounter;
 import com.ecaporali.trafficcounter.services.MetricService;
 
 import java.util.List;
 
+import static com.ecaporali.trafficcounter.config.AppConfig.CONTIGUOUS_LEAST_HALF_HOURS_COUNT;
+import static com.ecaporali.trafficcounter.config.AppConfig.TOP_HALF_HOURS_COUNT;
 import static com.ecaporali.trafficcounter.utils.AssertUtils.checkNonNull;
 
-public class TrafficCounterController {
+public class LogCounterController {
 
-    private AppConfig appConfig;
     private MetricService metricService;
 
-    public TrafficCounterController(AppConfig appConfig, MetricService metricService) {
-        checkNonNull(appConfig, "appConfig cannot be null");
-        checkNonNull(metricService, "metricService cannot be null");
-        this.appConfig = appConfig;
+    public LogCounterController(MetricService metricService) {
+        checkNonNull(metricService, "TrafficCounterController", "metricService cannot be null");
         this.metricService = metricService;
     }
 
     public CalculationResult calculateResults(List<LogCounter> logCounters) {
-        checkNonNull(logCounters, "logCounters cannot be null");
+        checkNonNull(logCounters, "TrafficCounterController.calculateResults", "logCounters cannot be null");
         return CalculationResult.Builder.newInstance()
                 .withTotalCarsCount(metricService.calculateTotalCarsCount(logCounters))
                 .withTotalCarsPerDay(metricService.calculateTotalCarsInEachDay(logCounters))
-                .withTopNHalfHours(metricService.calculateTopNHalfHours(logCounters, appConfig.TOP_HALF_HOURS_COUNT))
-                .withContiguousNLeastHalfHours(metricService.calculateContiguousNLeastHalfHours(logCounters, appConfig.CONTIGUOUS_LEAST_HALF_HOURS_COUNT))
+                .withTopNHalfHours(metricService.calculateTopNHalfHours(logCounters, TOP_HALF_HOURS_COUNT))
+                .withContiguousNLeastHalfHours(metricService.calculateContiguousNLeastHalfHours(logCounters, CONTIGUOUS_LEAST_HALF_HOURS_COUNT))
                 .build();
     }
 }
