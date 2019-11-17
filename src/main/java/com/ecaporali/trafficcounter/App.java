8bda7346ec14f3ef29c039eaca9fb2071cc228d4
Enrico Caporali <enrico.caporali@gmail.com>
2019-11-17 21:38:48 +1100

    Renamed Main.java to App.java

--- src/main/java/com/ecaporali/trafficcounter/App.java
+++ src/main/java/com/ecaporali/trafficcounter/App.java
@@ -1,7 +1,6 @@
 package com.ecaporali.trafficcounter;
 
-import com.ecaporali.trafficcounter.config.AppConfig;
-import com.ecaporali.trafficcounter.controllers.TrafficCounterController;
+import com.ecaporali.trafficcounter.controllers.LogCounterController;
 import com.ecaporali.trafficcounter.models.CalculationResult;
 import com.ecaporali.trafficcounter.models.LogCounter;
 import com.ecaporali.trafficcounter.services.LogFileReaderService;
@@ -13,13 +12,13 @@ import java.util.List;
 
 import static java.lang.String.format;
 
-public class Main {
+public class App {
 
     public static void main(String[] args) {
         String filename = "logbook.txt";
         try {
             List<LogCounter> logCounters = LogFileReaderService.readLogFile(filename);
-            TrafficCounterController trafficCounterController = new TrafficCounterController(new AppConfig(), new MetricService());
+            LogCounterController trafficCounterController = new LogCounterController(new MetricService());
             CalculationResult result = trafficCounterController.calculateResults(logCounters);
             System.out.println(result);
         } catch (IOException | URISyntaxException e) {
